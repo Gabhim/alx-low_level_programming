84dd1d10af7aba3f74898eb1fb2587604f820fe1
Gabriel Ibrahim <giotechict@gmail.com>
2022-09-09 16:42:30 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*program that print the string*/
-int main(void)
+int main(void) /*this returns zero at the end of the code*/
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
