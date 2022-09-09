5dca70f5755f7a5727510fd7ca687411a15b2f09
Gabriel Ibrahim <giotechict@gmail.com>
2022-09-09 16:34:23 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*program that print the string*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
