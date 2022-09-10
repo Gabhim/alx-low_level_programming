7589edc4e23a7e4e73dba6fd722e2724c326ed29
Gabriel Ibrahim <giotechict@gmail.com>
2022-09-09 17:24:22 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*program that print the string*/
-int main(void) /*this returns zero at the end of the code*/
+
+/**
+* main - prints the phrase "Programming is like building a multilingual puzzle
+* Return: 0 if exited properly, non-zero otherwise
+* */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
