7e80689e5dd8dc24240940b3f028ed8285705e57
Gabriel Ibrahim <giotechict@gmail.com>
2022-09-09 17:37:35 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * main - print the string "with proper grammar, but the outcome is a piece of art,
+ * Return will return 0 if properly done
+*/
 int main(void)
 {
 	/*Display the string in quote*/
