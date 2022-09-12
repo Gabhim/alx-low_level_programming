925cb017c00146466ddf05e82ff1e68e459c829e
Gabriel Ibrahim <giotechict@gmail.com>
2022-09-12 16:06:10 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - print the string "with proper grammar, but the outcome is a piece of art,
+ * main - print the string "with proper grammar, but the
+ * outcome is a piece of art,
  * Return:  will return 0 if properly done
 */
 int main(void)
